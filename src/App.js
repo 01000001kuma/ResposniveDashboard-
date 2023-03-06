a9b13ec8eab880fce5f126f6144a82589a718950
01000001kuma <a_illera@yahoo.com>
2023-03-06 19:25:48 +0100

    sidebar logic

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
 
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider'
+
 
 import './App.css'
 
 const App = () => {
 
-  const activeMenu = true;
-
+  const { activeMenu, setActiveMenu } = useStateContext();
+  
   return (
     <div>
       <BrowserRouter>
@@ -27,11 +29,11 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'> 
-              {Sidebar}
+              <Sidebar />
             </div>
           ) : (
             <div className='w-0 dark:bg-secondary-dark.bg'>
-              {Sidebar} 
+              <Sidebar />
             </div>
           )}
           <div className={
