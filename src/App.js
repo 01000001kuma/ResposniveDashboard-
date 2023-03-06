eaed71b736970a939905ee8423af4bcb956e85e7
01000001kuma <a_illera@yahoo.com>
2023-03-06 18:11:08 +0100

    wip sidebar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 
+import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
 
-import { Navbar, Footer, Sidebar, ThemeSettings} from './';
-import { ECommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
 
 
 import './App.css'
@@ -46,8 +46,8 @@ const App = () => {
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path='/' element={<ECommerce />} />
-              <Route path='/ecommerce' element={<ECommerce />} />
+              <Route path='/' element={<Ecommerce />} />
+              <Route path='/ecommerce' element={<Ecommerce />} />
 
               {/* Pages */}
               <Route path='/orders' element={<Orders  />} />
