ceffffdae88d829926fdee263d69e4af32fc213c
01000001kuma <a_illera@yahoo.com>
2023-03-09 17:01:43 +0100

    wip cards

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,7 +9,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 
 
-const Ecommer = () => {
+const Ecommerce = () => {
   return (
     <div className='mt-12' >
       <div className='flex flex-wrap lg:flex-nowrap justify-center' >
@@ -24,12 +24,32 @@ const Ecommer = () => {
             <Button color='white' bgColor='blue' text='Download' borderRadius='10px' size='md' />            
           </div>
         </div>
-        <div>
-          
+        <div className='flex m-3 flex-wrap justify-center gap-1 items-center' >
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'>
+              
+              <button type='button' style={{color: item.iconColor, backgroundColor: item.iconBg}} className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-x1' >
+                {item.icon} 
+              </button>
+              <p className='mt-3' >
+                <span className='text-lg font-semibold'>
+                  {item.amount}
+                </span>
+                <span className={`text-sm text-${item.pcColor} m1-2`} >
+                  {item.percentatge}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400 mt-1'>
+                {item.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
 
-export default Ecommer
+export default Ecommerce;
