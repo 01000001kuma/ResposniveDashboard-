2d315b813cba35938dd041ee94b748d3e08c4545
01000001kuma <a_illera@yahoo.com>
2023-03-12 13:53:49 +0100

    context for themes mode and logic

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -14,13 +14,27 @@ export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
     const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('light');
+    
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        
+        localStorage.setItem('themeMode ', e.target.value);
+    }
+
+    const setColor = (e) => {
+        setCurrentColor(e.target.value);
+        
+        localStorage.setItem('colorMode ', e.target.value);
+    }
     
     const handleClick = (cliked) =>  {
         setIsClicked({...initialState, [cliked]: true}); 
     }
 
     return (
-        <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize,  }}>
+        <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize,   }}>
             {children}
         </StateContext.Provider>
 
