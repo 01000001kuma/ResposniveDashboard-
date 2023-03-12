932187e804f7bc55fa83f3075e2d6aed0fcac995
01000001kuma <a_illera@yahoo.com>
2023-03-12 12:09:18 +0100

    starting importing for calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { ScheduleComponent, ViewDirective, ViewsDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+
+import { scheduleData } from '../data/dummy'
+import { Header } from '../components/Header'
+
 
 const Calendar = () => {
   return (
