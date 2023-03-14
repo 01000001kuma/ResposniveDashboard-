90dab2753712b6c59f5a93a39d6935d789fb5d98
01000001kuma <a_illera@yahoo.com>
2023-03-14 18:27:55 +0100

    dark mode ready

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ import './App.css'
 
 const App = () => {
 
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
   
   return (
-    <div>
+    <div className= { currentMode === 'Dark' ? 'dark' : '' }>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4 style={{ zIndex: '1000'}}">
             <TooltipComponent content="Settings" position="Top">
-              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' onClick={() => setThemeSettings(true)} style={{background: currentColor, borderRadius: '50%'}}>
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' onClick={() => setThemeSettings(true)} style={{background: currentColor , borderRadius: '50%'}}>
                 <FiSettings />
               </button>
             </TooltipComponent>
@@ -37,7 +37,10 @@ const App = () => {
             </div>
           )}
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ?  'md:ml-72 ': 'flex-2'}`            
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+            ${activeMenu 
+              ? 'md:ml-72 '
+              : 'flex-2'}`            
           }>
 
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
